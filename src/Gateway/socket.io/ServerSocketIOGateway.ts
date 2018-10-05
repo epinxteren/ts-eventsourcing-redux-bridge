a5927a8eb0567f7c1a13aca694c3bd576f907007
Eric Pinxteren <epinxteren@ibuildings.nl>
2018-10-05 14:16:14 +0200

    Add connect and disconnect observables

--- src/Gateway/socket.io/ServerSocketIOGateway.ts
+++ src/Gateway/socket.io/ServerSocketIOGateway.ts
@@ -1,7 +1,7 @@
 import { ServerGatewayInterface } from '../ServerGatewayInterface';
 import { Socket } from 'socket.io';
 import { EMPTY, fromEvent, Observable, Subject } from 'rxjs';
-import { catchError, finalize, share, switchMap, takeUntil } from 'rxjs/operators';
+import { catchError, finalize, map, mapTo, share, switchMap, takeUntil } from 'rxjs/operators';
 import { SerializerInterface } from '../../Serializer/SerializerInterface';
 import { SerializableAction } from '../../Redux/SerializableAction';
 import { ServerGatewayMessage } from '../ValueObject/ServerGatewayMessage';
@@ -9,30 +9,53 @@ import { NodeJSEventEmitterGateway } from '../node.js/NodeJSEventEmitterGateway'
 
 export interface ServerSocketIOGatewayMetadata {
   client: Socket;
-  clientGateway: ServerGatewayInterface;
+  clientGateway: ServerGatewayInterface<ServerSocketIOGatewayMetadata>;
 }
 
+export type ServerSocketIOGatewayMessage = ServerGatewayMessage<ServerSocketIOGatewayMetadata>;
+
+/**
+ * Connect to different clients, client errors will be passed to the error stream.
+ */
 export class ServerSocketIOGateway implements ServerGatewayInterface<ServerSocketIOGatewayMetadata> {
 
   private readonly warnings$: Subject<Error> = new Subject();
   private readonly message$: Observable<ServerGatewayMessage<ServerSocketIOGatewayMetadata>>;
   private readonly broadcastGateway: NodeJSEventEmitterGateway;
+  private readonly connections$: Observable<ServerSocketIOGatewayMetadata>;
+  private readonly disconnect$: Observable<ServerSocketIOGatewayMetadata>;
 
   constructor(emitter: NodeJS.EventEmitter, serializer: SerializerInterface) {
-    const connections$ = fromEvent<Socket>(emitter, 'connection');
-    this.message$ = connections$.pipe(
-      switchMap((socket) => {
-        const metadata: ServerSocketIOGatewayMetadata = { client: socket, clientGateway: undefined } as any;
-        const clientGateway = new NodeJSEventEmitterGateway(emitter, serializer, metadata);
-        metadata.clientGateway = clientGateway;
-        const subscription = clientGateway.warnings().subscribe(this.warnings$);
-        return clientGateway
+    this.connections$ = fromEvent<Socket>(emitter, 'connection')
+      .pipe(
+        map((socket) => {
+          const metadata: ServerSocketIOGatewayMetadata = { client: socket, clientGateway: undefined } as any;
+          const clientGateway = new NodeJSEventEmitterGateway(emitter, serializer, metadata);
+          metadata.clientGateway = clientGateway as any;
+          return metadata as any;
+        }),
+        share(),
+      );
+
+    this.disconnect$ = this.connections$.pipe(
+      switchMap((metadata: ServerSocketIOGatewayMetadata) => {
+        return fromEvent(metadata.client, 'disconnect').pipe(
+          mapTo(metadata),
+        );
+      }),
+      share(),
+    );
+
+    this.message$ = this.connections$.pipe(
+      switchMap((metadata: ServerSocketIOGatewayMetadata) => {
+        const subscription = metadata.clientGateway.warnings().subscribe(this.warnings$);
+        return metadata.clientGateway
           .listen()
           .pipe(
-            takeUntil(fromEvent(socket, 'disconnect')),
+            takeUntil(fromEvent(metadata.client, 'disconnect')),
             finalize(() => {
               subscription.unsubscribe();
-              socket.disconnect();
+              metadata.client.disconnect();
             }),
             catchError((e) => {
               this.warnings$.next(e);
@@ -50,8 +73,16 @@ export class ServerSocketIOGateway implements ServerGatewayInterface<ServerSocke
     return this.broadcastGateway.emit(action);
   }
 
-  public listen(): Observable<ServerGatewayMessage<ServerSocketIOGatewayMetadata>> {
+  public listen(): Observable<ServerSocketIOGatewayMessage> {
     return this.message$;
+  }
+
+  public connections(): Observable<ServerSocketIOGatewayMetadata> {
+    return this.connections$;
+  }
+
+  public disconnects(): Observable<ServerSocketIOGatewayMetadata> {
+    return this.disconnect$;
   }
 
   public warnings(): Observable<Error> {
